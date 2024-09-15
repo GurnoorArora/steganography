5bc1676b2513259b386290daf0f792d650bca1cc
LEVIATHAN0145 <127146393+LEVIATHAN0145@users.noreply.github.com>
2024-09-16 01:18:01 +0530

    Update embed.py
    the code has been completed to change the lsb's from RGB binary values

--- embed.py
+++ embed.py
@@ -1,8 +1,11 @@
 import obtain_pixel
 import convert
 
-pixels = obtain_pixel.pixels
+string = obtain_pixel.string
 bin_list = convert.bin_list
+#print(bin_list)
+
+pixels = obtain_pixel.pixels
 sel_pixels=obtain_pixel.sel_pixels
 bin_RGB=[]
 for i in range(0,len(sel_pixels)):
@@ -12,7 +15,19 @@ for i in range(0,len(sel_pixels)):
     RGB_BIN=[]    
     for values in RGB:
         temp=bin(values)
-        RGB_BIN.append(int(temp[2::]))
+        RGB_BIN.append(temp[2::].zfill(7))
     bin_RGB.append(RGB_BIN)
-    
-print(bin_RGB)    
+#print("RGB list before embedding: ",bin_RGB)
+RGB_count=0
+for bin_letter in bin_list:
+    bin_letter=str(bin_letter)
+    R_embed=bin_letter[0:2]
+    G_embed=bin_letter[2:4]
+    B_embed=bin_letter[4:7]
+
+    bin_RGB[RGB_count][0]=bin_RGB[RGB_count][0][0:len(bin_RGB[RGB_count][0])-2]+R_embed
+    bin_RGB[RGB_count][1]=bin_RGB[RGB_count][1][0:len(bin_RGB[RGB_count][0])-2]+G_embed
+    bin_RGB[RGB_count][2]=bin_RGB[RGB_count][2][0:len(bin_RGB[RGB_count][0])-3]+B_embed
+    RGB_count+=1
+
+#print("RGB list after embedding: ",bin_RGB)    
